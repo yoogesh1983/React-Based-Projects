f72aa8b0dd97c02a18aea216e019537e1f640220
Yoogesh <yoogesh1983@gmail.com>
2018-12-25 18:56:38 -0500

    Changing Name of Projects

--- TWMContact_SSR/src/components/contactManager/__fixtures__/allContactSchema.js
+++
@@ -1,7 +0,0 @@
-export default {
-  "definitions": {},
-  "$schema": "http://json-schema.org/draft-07/schema#",
-  "$id": "http://example.com/root.json",
-  "type": "object",
-  "title": "The Root Schema"
-}
